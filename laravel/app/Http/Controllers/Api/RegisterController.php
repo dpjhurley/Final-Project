3da7137a3128d3d1b3e48e0f83224fbb13c65134
dpjhurley <dpjhurley@gmail.com>
2020-07-08 13:26:44 +0200

    react-router prep

--- laravel/app/Http/Controllers/Api/RegisterController.php
+++ laravel/app/Http/Controllers/Api/RegisterController.php
@@ -17,17 +17,17 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
-    protected function validator(Request $request, array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'surname' => ['required', 'string', 'max:255'],
-            'date_of_birth' => ['required', 'date'],
-            'gender' => ['required', 'string'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
+    // protected function validator(Request $request, array $data)
+    // {
+    //     return Validator::make($data, [
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'surname' => ['required', 'string', 'max:255'],
+    //         'date_of_birth' => ['required', 'date'],
+    //         'gender' => ['required', 'string'],
+    //         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    //         'password' => ['required', 'string', 'min:8', 'confirmed'],
+    //     ]);
+    // }
 
     /**
      * Create a new user instance after a valid registration.
--- laravel/resources/js/Components/App.jsx
+++ laravel/resources/js/Components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import AccountArea from './auth/AccountArea.jsx'
 import TopNav from './Topnav.jsx';
 import Navbar from './Navbar.jsx';
