d85547f60f249f6aa9339342be9211f8aca16c34
dpjhurley <dpjhurley@gmail.com>
2020-07-13 16:08:14 +0200

    fixed seeder for stock

--- laravel/database/migrations/2020_07_13_113703_create_stocks_table.php
+++ laravel/database/migrations/2020_07_13_113703_create_stocks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStockTable extends Migration
+class CreateStocksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateStockTable extends Migration
      */
     public function up()
     {
-        Schema::create('stock', function (Blueprint $table) {
+        Schema::create('stocks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('shoe_id')->nullable();
             $table->foreignId('size_id')->nullable();
@@ -29,6 +29,6 @@ class CreateStockTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('stock');
+        Schema::dropIfExists('stocks');
     }
 }
